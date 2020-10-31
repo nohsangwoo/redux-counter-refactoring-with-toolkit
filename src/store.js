2a505d0672bf2327890f4f7b5355184662e28a68
nohsangwoo <nsgr12@naver.com>
2020-10-31 23:52:56 +0900

    configureStore

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 
@@ -20,7 +20,9 @@ const reducer = createReducer([], {
   },
 });
 
-const store = createStore(reducer);
+// configureStore는 여러 middleware랑 같이 사용가능
+// 이번경우에는 구글 확장프로그램에서 redux devtools 라는곳에서 state의 상태를 확인하고 이전의 log들을 불러와 확인도 가능함, 이전에 실행했던 순서대로 돌아갈수도있음
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
