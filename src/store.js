75e2a158e7035e53d69bf9f619c51984cf6a41c9
nohsangwoo <nsgr12@naver.com>
2020-10-31 23:37:28 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,24 +1,24 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 
 const deleteToDo = createAction("DELETE");
 
-// console.log(addToDo.type, deleteToDo());
+// switch를 사용하지 않고 reducer를 세팅할수있음
 
-const reducer = (state = [], action) => {
-  console.log(action);
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      console.log("delete action");
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    // return 할때에는 완전히 새로운 state를 return 해야한다.
+    //  새로운 state가 아닐때에는 return하지 않는다.
+    // 그래도 immer라는 것이  return [{ text: action.payload, id: Date.now() }, ...state]; 이런식으로 작동하게 만들어줌
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => {
+    // 여기선 완전히 새로운 state니깐 return 해준다
+    return state.filter((toDo) => toDo.id !== action.payload);
+  },
+});
 
 const store = createStore(reducer);
 
